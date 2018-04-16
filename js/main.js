dc5a6f160b9223bc0b888415c626c3c834923c8c
bartoszpieczynski <35772248+bartoszpieczynski@users.noreply.github.com>
2018-04-17 00:08:07 +0200

    more changes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 {
-   $('.skills__main--icon').on('mouseover', function() {
+   $('.skills__main__icon').on('mouseover', function() {
       $(this).addClass('colored');
    });
-   $('.skills__main--icon').on('mouseout', function() {
+   $('.skills__main__icon').on('mouseout', function() {
       $(this).removeClass('colored');
    });
-   $('.skills__learning--icon').on('mouseover', function() {
+   $('.skills__learning__icon').on('mouseover', function() {
       $(this).addClass('colored');
    });
-   $('.skills__learning--icon').on('mouseout', function() {
+   $('.skills__learning__icon').on('mouseout', function() {
+      $(this).removeClass('colored');
+   });
+   $('.projects__github--icon').on('mouseover', function() {
+      $(this).addClass('colored');
+   });
+   $('.projects__github--icon').on('mouseout', function() {
       $(this).removeClass('colored');
    });
 }
