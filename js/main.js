1cba42d030354e726c74a9fb40eb49c9d0ca3c13
bartoszpieczynski <35772248+bartoszpieczynski@users.noreply.github.com>
2018-04-17 22:10:14 +0200

    header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
+
+
+let typed = new Typed('#header-1', {
+   startDelay: 2000,
+   strings: [`Hello!`, "Welcome to my worl", `Welcome to my portfolio.`],
+   typeSpeed: 50,
+   backSpeed: 10,
+   smartBackspace: true,
+   showCursor: false
+});
+
+
+let typedSmall = new Typed('#header-2', {
+   startDelay: 7000,
+   strings: ['Scroll down to get to know me better.'],   
+   typeSpeed: 40,  
+   backSpeed: 10,
+   smartBackspace: true,
+   showCursor: false
+});
+
+
+
+
+
+
 {
    $('.skills__main__icon').on('mouseover', function() {
       $(this).addClass('colored');
