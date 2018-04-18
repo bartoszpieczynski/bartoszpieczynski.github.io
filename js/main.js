c8dce059b7f67e01b40fcf418932be70a7b85844
bartoszpieczynski <35772248+bartoszpieczynski@users.noreply.github.com>
2018-04-19 01:56:59 +0200

    new onepage scroll plugin

--- js/main.js
+++ js/main.js
@@ -67,79 +67,22 @@ function oldActive() {
      }
  }
  
- function backgroundTransition() {
-   let active = document
-     .getElementsByClassName("active")[0]
-     .getAttribute("data-index");
-   if (active == 1) {
+ function backgroundTransition(nextIndex) {
+   if (nextIndex == 1) {
      $(".about__text__eraser").css("border-color", "#2E3A87");
      $("body").css("background-color", "#2E3A87");
-   } else if (active == 2) {
+   } else if (nextIndex == 2) {
      $(".about__text__eraser").css("border-color", "#0097A7");
      $("body").css("background-color", "#0097A7");
-   } else if (active == 3) {
+   } else if (nextIndex == 3) {
      $(".about__text__eraser").css("border-color", "#00BCD4");
      $("body").css("background-color", "#00BCD4");
-   } else if (active == 4) {
+   } else if (nextIndex == 4) {
      $("body").css("background-color", "#009688");
-   } else if (active == 5) {
+   } else if (nextIndex == 5) {
      $("body").css("background-color", "#1972A4");
    }
  }
 
- (function($) { 
-  $.fn.swipeEvents = function() {
-    return this.each(function() {
-      
-      var startX,
-          startY,
-          $this = $(this);
-      
-      $this.bind('touchstart', touchstart);
-      
-      function touchstart(event) {
-        var touches = event.originalEvent.touches;
-        if (touches && touches.length) {
-          startX = touches[0].pageX;
-          startY = touches[0].pageY;
-          $this.bind('touchmove', touchmove);
-          $this.bind('touchend', touchend);
-        }
-        event.preventDefault();
-      }
-      
-      function touchmove(event) {
-        var touches = event.originalEvent.touches;
-        if (touches && touches.length) {
-          var deltaX = startX - touches[0].pageX;
-          var deltaY = startY - touches[0].pageY;
-          
-          if (deltaX >= 50) {
-            $this.trigger("swipeLeft");
-          }
-          if (deltaX <= -50) {
-            $this.trigger("swipeRight");
-          }
-          if (deltaY >= 50) {
-            $this.trigger("swipeUp");
-          }
-          if (deltaY <= -50) {
-            $this.trigger("swipeDown");
-          }
-          if (Math.abs(deltaX) >= 50 || Math.abs(deltaY) >= 50) {
-            $this.unbind('touchmove', touchmove);
-            $this.unbind('touchend', touchend);
-          }
-        }
-        event.preventDefault();
-      }
-      
-      function touchend(event) {
-        $this.unbind('touchmove', touchmove);
-        event.preventDefault();
-      }
-      
-    });
-  };
-})(jQuery);
+
 
