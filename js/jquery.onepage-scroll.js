e180a652f254993e87f56874712d8cd84efb60aa
bartoszpieczynski <35772248+bartoszpieczynski@users.noreply.github.com>
2018-04-19 00:35:04 +0200

    scroll

--- js/jquery.onepage-scroll.js
+++ js/jquery.onepage-scroll.js
@@ -33,59 +33,61 @@
   /*  Credit: Eike Send for the awesome swipe event */
   /*------------------------------------------------*/
 
-  $.fn.swipeEvents = function() {
-    return this.each(function() {
-
-      var startX,
-          startY,
-          $this = $(this);
-
-      $this.bind('touchstart', touchstart);
-
-      function touchstart(event) {
-        var touches = event.originalEvent.touches;
-        if (touches && touches.length) {
-          startX = touches[0].pageX;
-          startY = touches[0].pageY;
-          $this.bind('touchmove', touchmove);
-          $this.bind('touchend', touchend);
+  (function($) { 
+    $.fn.swipeEvents = function() {
+      return this.each(function() {
+        
+        var startX,
+            startY,
+            $this = $(this);
+        
+        $this.bind('touchstart', touchstart);
+        
+        function touchstart(event) {
+          var touches = event.originalEvent.touches;
+          if (touches && touches.length) {
+            startX = touches[0].pageX;
+            startY = touches[0].pageY;
+            $this.bind('touchmove', touchmove);
+            $this.bind('touchend', touchend);
+          }
+          event.preventDefault();
         }
-        event.preventDefault();
-      }
-
-      function touchmove(event) {
-        var touches = event.originalEvent.touches;
-        if (touches && touches.length) {
-          var deltaX = startX - touches[0].pageX;
-          var deltaY = startY - touches[0].pageY;
-
-          if (deltaX >= 50) {
-            $this.trigger("swipeLeft");
+        
+        function touchmove(event) {
+          var touches = event.originalEvent.touches;
+          if (touches && touches.length) {
+            var deltaX = startX - touches[0].pageX;
+            var deltaY = startY - touches[0].pageY;
+            
+            if (deltaX >= 50) {
+              $this.trigger("swipeLeft");
+            }
+            if (deltaX <= -50) {
+              $this.trigger("swipeRight");
+            }
+            if (deltaY >= 50) {
+              $this.trigger("swipeUp");
+            }
+            if (deltaY <= -50) {
+              $this.trigger("swipeDown");
+            }
+            if (Math.abs(deltaX) >= 50 || Math.abs(deltaY) >= 50) {
+              $this.unbind('touchmove', touchmove);
+              $this.unbind('touchend', touchend);
+            }
           }
-          if (deltaX <= -50) {
-            $this.trigger("swipeRight");
-          }
-          if (deltaY >= 50) {
-            $this.trigger("swipeUp");
-          }
-          if (deltaY <= -50) {
-            $this.trigger("swipeDown");
-          }
-          if (Math.abs(deltaX) >= 50 || Math.abs(deltaY) >= 50) {
-            $this.unbind('touchmove', touchmove);
-            $this.unbind('touchend', touchend);
-          }
+          event.preventDefault();
         }
-        event.preventDefault();
-      }
-
-      function touchend(event) {
-        $this.unbind('touchmove', touchmove);
-        event.preventDefault();
-      }
-
-    });
-  };
+        
+        function touchend(event) {
+          $this.unbind('touchmove', touchmove);
+          event.preventDefault();
+        }
+        
+      });
+    };
+  })(jQuery);
 
   $.fn.onepage_scroll = function(options) {
     var settings = $.extend({}, defaults, options),
